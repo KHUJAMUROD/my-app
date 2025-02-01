ff9ea47ac44b9f6032a67ee3b13acdf433c563e2
HUNTER <xujanov.xujamurod@gmail.com>
2025-02-01 19:33:52 +0500

    classwork

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, memo} from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-function useInputWithValidate(initialValue) {
-    const [value, setValue] = useState(initialValue);
+// function useInputWithValidate(initialValue) {
+//     const [value, setValue] = useState(initialValue);
 
-    const onChange = event => {
-        setValue(event.target.value);
-    }
+//     const onChange = event => {
+//         setValue(event.target.value);
+//     }
 
-    const validateInput = () => {
-        return value.search(/\d/) >= 0 ? true : false;
+//     const validateInput = () => {
+//         return value.search(/\d/) >= 0 ? true : false;
 
-    }
+//     }
 
-    return { value, onChange, validateInput }
+//     return { value, onChange, validateInput }
+// }
+
+function propsCompare (prevProps, nextProps) {
+    return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text;
 }
 
-const Form = () => {
+const Form = memo((props) => {
+    console.log('render');
+    
 
     // const [text, setText] = useState('');
 
     // const [textArea, setTextArea] = useState('');
 
-    const input = useInputWithValidate('')
-    const textArea = useInputWithValidate('')
+    // const input = useInputWithValidate('')
+    // const textArea = useInputWithValidate('')
 
 
-    const color = input.validateInput() ? 'text-danger' : null;
+    // const color = input.validateInput() ? 'text-danger' : null;
 
-    const myRef = useRef(1);
+    // const myRef = useRef(1);
 
     // useEffect(() => {
     //   myRef.current
@@ -45,22 +51,23 @@ const Form = () => {
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input type="text" value={`${input.value} / ${textArea.value}`} className='form-control' readOnly />
+                    {/* <input type="text" value={`${input.value} / ${textArea.value}`} className='form-control' readOnly /> */}
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
                     <input
-                        onChange={input.onChange}
+                        // onChange={input.onChange}
                         type="email"
-                        value={input.value}
-                        className={`form-control ${color}`}
+                        value={props.mail}
+                        className='form-control'
+                        // className={`form-control ${color}`}
                         id="exampleFormControlInput1"
                         placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
                     <textarea
-                        onChange={textArea.onChange}
-                        value={textArea.value}
-                        onClick={() => myRef.current + 1}
+                        // onChange={textArea.onChange}
+                        value={props.text}
+                        // onClick={() => myRef.current + 1}
                         className="form-control"
                         id="exampleFormControlTextarea1"
                         rows="3"></textarea>
@@ -68,11 +75,27 @@ const Form = () => {
             </form>
         </Container>
     )
-};
+}, propsCompare)
+
+
 
 function App() {
+
+    const [data, setData] = useState({
+        mail: "name@example.com",
+        text: "some text"
+    })
+
     return (
-        <Form />
+        <>
+            <Form mail={data.mail} text={data.text} />
+            <button onClick={() => setData({
+                mail: "name@example.com",
+                text: "some text"
+            })}>
+                Click me
+            </button>
+        </>
     );
 };
 
